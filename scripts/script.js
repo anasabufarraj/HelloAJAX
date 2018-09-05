5f4c4a73f94d18e4b12d68316a91ceacc4ad15e9
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-09-05 11:34:12 +0300

    Backing up.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 var button = document.getElementById('button');
-// functions
-function loadText() {
-    // console.log('#button clicked')
+var paragraph = document.getElementById('text');
+function loadDoc() {
+    var XHttpRequest = new XMLHttpRequest();
+    XHttpRequest.open('GET', 'sample.txt', true);
+    XHttpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            paragraph.innerText = this.responseText;
+            console.log('Status', XHttpRequest.status);
+            console.log(XHttpRequest.getAllResponseHeaders());
+        }
+    };
+    XHttpRequest.send();
 }
-button.addEventListener('click', loadText);
+button.addEventListener('click', loadDoc);
+//# sourceMappingURL=script.js.map
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,11 +1,31 @@
+// © Anas Abu Farraj
+// AJAX Training Project
+// September 4, 2018
+
 var button = document.getElementById('button');
+var paragraph = document.getElementById('text');
 
-// functions
-function loadText() {
-  // console.log('#button clicked')
+function loadDoc() {
+  // creating XMLHttpRequest (XHR) object
+  var XHttpRequest = new XMLHttpRequest();
 
+  // initializing request
+  // open(method, url, async, user, psw)
+  XHttpRequest.open('GET', 'sample.txt', true);
+
+  // get data when transaction completes successfully.
+  XHttpRequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      paragraph.innerText = this.responseText;
+      console.log('Status', XHttpRequest.status);
+      console.log(XHttpRequest.getAllResponseHeaders());
+    }
+  };
+
+  // sending request
+  XHttpRequest.send();
 }
 
-button.addEventListener('click', loadText);
-
-
+// catch button click event
+// trigger function
+button.addEventListener('click', loadDoc);
